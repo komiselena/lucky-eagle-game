5e7215ab2eb78cfb9bce37c8b2eede2b7f25798a
komiselena <komiselen@yandex.ru>
2025-04-25 13:27:25 +0300

    Initial Commit

---
+++ Lucky Eagle GameTests/Lucky_Eagle_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lucky_Eagle_GameTests.swift
+//  Lucky Eagle GameTests
+//
+//  Created by Mac on 25.04.2025.
+//
+
+import Testing
+@testable import Lucky_Eagle_Game
+
+struct Lucky_Eagle_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
